c512c6ff7211fb43fc2c52241a9ecd9d0e442e9a
stanhjr <stanislav.o@cybearsoft.com>
2022-06-21 00:49:00 +0300

    admin register

--- site_track/admin.py
+++ site_track/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MyUser, SaleAds, ImageInGallery
 
+admin.site.register(MyUser)
+admin.site.register(SaleAds)
+admin.site.register(ImageInGallery)
 # Register your models here.
