798b3dc16fb9d23135f37daa0fd53ddf2ce10b64
stanhjr <stanislav.o@cybearsoft.com>
2022-06-27 23:13:19 +0300

    hotfix

--- django_site_track/settings.py
+++ django_site_track/settings.py
@@ -57,7 +57,12 @@ MIDDLEWARE = [
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join('static'), )
+STATIC_ROOT = '/root/django_site_track/static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/'
 
 ROOT_URLCONF = 'django_site_track.urls'
@@ -128,12 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = (os.path.join('static'), )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
