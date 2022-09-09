a180d56a43ef266ba3717ece8e0005208e66bf3c
stanhjr <stanislav.o@cybearsoft.com>
2022-09-09 19:05:05 +0300

    debug false

--- django_site_track/settings.py
+++ django_site_track/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b3col@q^@xo!&l8foy=&60y9ly50l%s)6s$t^$6bff*4(fep)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
