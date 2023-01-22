d55515a12582d47c3ca9fdbb76faefc3dbdd566e
stanhjr <stanislav.o@cybearsoft.com>
2023-01-22 14:52:02 +0200

    fix faq

--- core/models.py
+++ core/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Faq(models.Model):
     title = models.CharField(max_length=1000)
     text = models.TextField(max_length=10000)
-    number = models.ImageField(default=1)
+    number = models.IntegerField(default=1)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ('number', )
 
 
 class DateSubscribe(models.Model):
